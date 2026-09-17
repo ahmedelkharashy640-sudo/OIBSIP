bace00fc47ee4057417feb4ca02219bdeac2d73d
𝓐𝓱𝓶𝓮𝓭_𝓔𝓵𝓴𝓱𝓪𝓻𝓪𝓼𝓱𝔂 <ahmedelkharashy640@gmail.com>
2026-09-18 02:57:53 +0300

    Add files via upload

--- WebDev_L1_LandingPage/assets/js/main.js
+++ WebDev_L1_LandingPage/assets/js/main.js
@@ -2,4 +2,27 @@ const themeToggleBtn = document.getElementById('themeToggleBtn');
 
 themeToggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('Glow-theme');
+}); 
+
+const navLinks = document.querySelectorAll('nav .container .body ul li a');
+const sections = document.querySelectorAll('.page-section');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').replace('#', '');
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      sections.forEach(sec => sec.classList.remove('active-section'));
+      
+      navLinks.forEach(l => l.parentElement.classList.remove('active'));
+
+      targetSection.classList.add('active-section');
+      this.parentElement.classList.add('active');
+
+      window.scrollTo({ top: 0, behavior: 'instant' });
+    }
+  });
 });
